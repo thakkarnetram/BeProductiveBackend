1d98872e6d56c66fd82976df6e1b49c5cc84fb72
thakkarnetram <thakkarnetram10@gmail.com>
2023-09-25 17:33:38 +0530

    adding and testing socket io

--- utils/socket.js
+++ utils/socket.js
@@ -1,20 +1,43 @@
-const socketIO = require("socket.io")
+const socketIO = require("socket.io");
+const Message = require("../models/Message");
 
-const socketConnection = (server) => {
-    const io = socketIO(server);
-    io.on('connection', (socket) => {
-        console.log("User connected")
-        // Msg from user
-        socket.on("chat message", (message) => {
-            console.log(`message received  ${message}`)
-            // Broadcast msg to users
-            io.emit("chat message",message)
-        })
-        // dc
-        socket.on("disconnect",()=>{
-            console.log("user disconnected")
-        })
-    })
-}
+let io; // Store the io instance globally
 
-module.exports=socketConnection
+// Initialize socket.io and store the instance in the io variable
+exports.socketConnection = (server) => {
+    io = socketIO(server);
+    io.on("connection", (socket) => {
+
+        console.log("User connected");
+
+// Handle socket events as needed
+        socket.on("chat message", async (data) => {
+            console.log(`Message received: ${data.message}`);
+
+            try {
+                // Create a new Message document
+                const newMessage = new Message({
+                    text: data.message,
+                    sentBy: data.userId, // Replace with the actual user ID
+                    channel: data.channelId, // Replace with the actual channel ID
+                });
+
+                // Save the message to MongoDB using await
+                const savedMessage = await newMessage.save();
+                console.log("Message saved:", savedMessage);
+
+                // Broadcast msg to users
+                io.emit("chat message", {
+                    message: data.message,
+                    sentBy: data.userId,
+                    channel: data.channelId,
+                });
+            } catch (err) {
+                console.error("Error saving message:", err);
+            }
+        });
+        socket.on("disconnect", () => {
+            console.log("User disconnected");
+        });
+    });
+};
