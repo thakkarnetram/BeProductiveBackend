c73470eb3dd31257ac23871b1f1dc60a7194a374
thakkarnetram <thakkarnetram10@gmail.com>
2024-10-05 22:16:56 +0530

    merge conflicts v2 fixed

--- app.js
+++ app.js
@@ -9,15 +9,14 @@ app.set("views", path.join(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 const authRoute = require("./src/router/authRoute");
 const actionRoute = require("./src/router/actionRouter");
-const pingRoute = require("./src/router/pingRoute")
-const profileRoute = require("./src/router/profileRouter")
-app.use("/auth", authRoute);
-app.use("/user", actionRoute);
-app.use("/",profileRoute)
 const pingRoute = require("./src/router/pingRoute");
+const profileRoute = require("./src/router/profileRouter");
 const inviteRoute = require("./src/router/inviteRoute");
 app.use("/auth", authRoute);
 app.use("/user", actionRoute);
+app.use("/auth", authRoute);
+app.use("/user", actionRoute);
 app.use("/invite", inviteRoute);
+app.use("/", profileRoute);
 app.use("/", pingRoute);
 module.exports = app;
