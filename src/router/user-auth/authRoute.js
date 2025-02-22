6b39ce10bb61a73de0b46489efcb3ac320ef2d10
thakkarnetram <thakkarnetram10@gmail.com>
2025-02-22 16:30:43 +0530

    rate limiting for authentication based requests

--- src/router/user-auth/authRoute.js
+++ src/router/user-auth/authRoute.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const authController = require("../../controllers/user-auth/authController");
+const rateLimiter = require('../../utils/rate-limiting/authRateLimiter')
 
 const router = express.Router();
 
-router.route("/api/v1/signup").post(authController.signup);
-router.route("/api/v1/login").post(authController.login);
+router.route("/api/v1/signup").post(rateLimiter.registerLimit, authController.signup);
+router.route("/api/v1/login").post(rateLimiter.loginLimit, authController.login);
 // router.route("/api/v1/google").post(authController.googleAuth);
-router.route("/api/v1/verify").get(authController.verifyEmail);
-router.route("/api/v1/reset").post(authController.resetPasswordLink);
+router.route("/api/v1/verify").get(rateLimiter.verifyEmailLimit, authController.verifyEmail);
+router.route("/api/v1/reset").post(rateLimiter.resetPasswordGenerateLinkLimit, authController.resetPasswordLink);
 router
     .route("/api/v1/reset/:_id")
-    .get(authController.resetPassword)
-    .post(authController.handlePasswordReset);
+    .get(rateLimiter.resetPasswordPageLimit, authController.resetPassword)
+    .post(rateLimiter.resetPasswordUpdatingLimit, authController.handlePasswordReset);
 
 
 module.exports = router;
