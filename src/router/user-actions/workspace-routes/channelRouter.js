ce8840d4504f85310bf2b6d65c1bb41a7ba9272b
thakkarnetram <thakkarnetram10@gmail.com>
2025-02-25 15:51:01 +0530

    added:limitation for workspace api

--- src/router/user-actions/workspace-routes/channelRouter.js
+++ src/router/user-actions/workspace-routes/channelRouter.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const channelController = require("../../../controllers/user-actions/workspace-actions/channelController");
 const authController = require("../../../controllers/user-auth/authController");
+const channelLimiter = require('../../../utils/rate-limiting/workSpaceRateLimiter')
 
 const router = express.Router();
 
 // CHANNELS
 router
-  .route("/api/v1/channels")
-  .get(authController.protect, channelController.getChannels);
+    .route("/api/v1/channels")
+    .get(channelLimiter.getChannelLimit, authController.protect, channelController.getChannels);
 router
-  .route("/api/v1/channel/:_id")
-  .get(authController.protect, channelController.getChannelById);
+    .route("/api/v1/channel/:_id")
+    .get(channelLimiter.getChannelLimit, authController.protect, channelController.getChannelById);
 router
-  .route("/api/v1/channel/latest")
-  .get(authController.protect, channelController.getLatestChannel);
+    .route("/api/v1/channel/latest")
+    .get(channelLimiter.getChannelLimit, authController.protect, channelController.getLatestChannel);
 router
-  .route("/api/v1/channel/add/:_id")
-  .post(authController.protect, channelController.createChannel);
+    .route("/api/v1/channel/add/:_id")
+    .post(channelLimiter.addChannelLimit, authController.protect, channelController.createChannel);
 
 module.exports = router;
--- src/router/user-actions/workspace-routes/messageRouter.js
+++ src/router/user-actions/workspace-routes/messageRouter.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const messageController = require("../../../controllers/user-actions/workspace-actions/messageController");
 const authController = require("../../../controllers/user-auth/authController");
+const messageLimiter = require('../../../utils/rate-limiting/workSpaceRateLimiter')
 
 const router = express.Router();
 
 // MESSAGES
 router
-  .route("/api/v1/message/:channel")
-  .get(authController.protect, messageController.getChannelMessageById);
+    .route("/api/v1/message/:channel")
+    .get(messageLimiter.messageLimit, authController.protect, messageController.getChannelMessageById);
 
 module.exports = router;
