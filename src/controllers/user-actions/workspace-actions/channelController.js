2a5ceda7621b0eb5cebb6f5cdb96135c587c11b0
thakkarnetram <thakkarnetram10@gmail.com>
2025-02-27 19:23:49 +0530

    feat : channel update and delete api added

--- src/controllers/user-actions/workspace-actions/channelController.js
+++ src/controllers/user-actions/workspace-actions/channelController.js
@@ -7,35 +7,36 @@ const channelCache = new NodeCache({
 });
 
 const asyncErrorHandler = require("../../../utils/error-handlers/AsyncErrorHandler");
+const {async} = require("seed");
 
 // CHANNEL SECTION
 exports.getChannels = asyncErrorHandler(async (req, res, next) => {
     const userId = req.user._id;
     if (!userId) {
-        return res.status(400).json({ message: "User ID not found" });
+        return res.status(400).json({message: "User ID not found"});
     }
     try {
         let channels;
-         channels = await Channel.find({
+        channels = await Channel.find({
             $or: [
-                { admin: userId }, // Channels where the user is the admin
-                { members: userId }, // Channels where the user is a member
+                {admin: userId}, // Channels where the user is the admin
+                {members: userId}, // Channels where the user is a member
             ],
         });
         if (!channels || channels.length === 0) {
             return res
                 .status(404)
-                .json({ message: "No channels found! Create one." });
+                .json({message: "No channels found! Create one."});
         }
-        if(channelCache.has(`channels:${userId}`)) {
+        if (channelCache.has(`channels:${userId}`)) {
             channels = JSON.parse(channelCache.get(`channels:${userId}`))
         } else {
-            channelCache.set(`channels:${userId}`,JSON.stringify(channels))
+            channelCache.set(`channels:${userId}`, JSON.stringify(channels))
         }
         return res.status(200).json(channels);
     } catch (e) {
         console.error(e);
-        return res.status(500).json({ message: "Internal Server Error" });
+        return res.status(500).json({message: "Internal Server Error"});
     }
 });
 
@@ -43,30 +44,30 @@ exports.getChannelById = asyncErrorHandler(async (req, res, next) => {
     const userId = req.user._id;
     const channelId = req.params._id;
     if (!userId) {
-        return res.status(400).json({ message: "User ID not found" });
+        return res.status(400).json({message: "User ID not found"});
     }
     try {
         let findChannel;
         findChannel = await Channel.findById(channelId);
         if (!findChannel) {
-            return res.status(404).json({ message: "Channel not found" });
+            return res.status(404).json({message: "Channel not found"});
         }
-        if(channelCache.has(`channelById:${userId}`)) {
+        if (channelCache.has(`channelById:${userId}`)) {
             findChannel = JSON.parse(channelCache.get(`channelById:${userId}`))
         } else {
-            channelCache.set(`channelById${userId}`,JSON.stringify(findChannel))
+            channelCache.set(`channelById${userId}`, JSON.stringify(findChannel))
         }
         // Check if the uscoer is either the admin or a member of the channel
         if (
             findChannel.admin.toString() !== userId &&
             !findChannel.members.includes(userId)
         ) {
-            return res.status(403).json({ message: "Permission denied" });
+            return res.status(403).json({message: "Permission denied"});
         }
         return res.status(200).json(findChannel);
     } catch (e) {
         console.error(e);
-        return res.status(500).json({ message: "Internal Server Error" });
+        return res.status(500).json({message: "Internal Server Error"});
     }
 });
 
@@ -77,44 +78,44 @@ exports.getLatestChannel = asyncErrorHandler(async (req, res, next) => {
         let latestChannel;
         latestChannel = await Channel.find({
             $or: [
-                { admin: userId }, // Channels where the user is the admin
-                { members: userId }, // Channels where the user is a member
+                {admin: userId}, // Channels where the user is the admin
+                {members: userId}, // Channels where the user is a member
             ],
         })
-            .sort({ createdAt: -1 })
+            .sort({createdAt: -1})
             .limit(2);
 
         if (!latestChannel) {
-            return res.status(404).json({ message: "No Channel found" });
+            return res.status(404).json({message: "No Channel found"});
         }
-        if(channelCache.has(`latestChannel:${userId}`)) {
+        if (channelCache.has(`latestChannel:${userId}`)) {
             latestChannel = JSON.parse(channelCache.get(`latestChannel:$userId`))
         } else {
-            channelCache.set(`latestChannel:${userId}`,JSON.stringify(latestChannel))
+            channelCache.set(`latestChannel:${userId}`, JSON.stringify(latestChannel))
         }
         return res.status(200).json(latestChannel);
     } catch (e) {
         console.log(e);
-        return res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({message: "Internal server error"});
     }
 });
 
 exports.createChannel = asyncErrorHandler(async (req, res, next) => {
     const userId = req.user._id;
-    const { channelName } = req.body;
+    const {channelName} = req.body;
 
     if (!channelName) {
-        return res.status(400).json({ message: "Channel name is required" });
+        return res.status(400).json({message: "Channel name is required"});
     }
     try {
-        const workspace = await Workspace.findById({ _id: req.params._id });
+        const workspace = await Workspace.findById({_id: req.params._id});
         if (!workspace) {
-            return res.status(404).json({ message: "No workspace found with Id" });
+            return res.status(404).json({message: "No workspace found with Id"});
         }
         // Create a new channel based on the request body
         const newChannel = new Channel({
             channelName,
-            workspace: { _id: workspace._id, workspaceName: workspace.workspace },
+            workspace: {_id: workspace._id, workspaceName: workspace.workspace},
             admin: userId,
         });
         await newChannel.save();
@@ -122,15 +123,59 @@ exports.createChannel = asyncErrorHandler(async (req, res, next) => {
             _id: newChannel._id,
             channelName: newChannel.channelName,
         });
-        channelCache.del( `latestChannel:${userId}`);
-        channelCache.del( `channels:${userId}`);
-        channelCache.del( `channelById:${userId}`);
+        channelCache.del(`latestChannel:${userId}`);
+        channelCache.del(`channels:${userId}`);
+        channelCache.del(`channelById:${userId}`);
         await workspace.save();
-        return res.status(201).json({ message: "Channel Created", newChannel });
+        return res.status(201).json({message: "Channel Created", newChannel});
     } catch (e) {
         console.error(e);
-        return res.status(500).json({ message: "Internal Server Error" });
+        return res.status(500).json({message: "Internal Server Error"});
     }
 });
 
-// TODO DELETE AND UPDATE CHANNELS
+exports.updateChannel = asyncErrorHandler(async (req, res, next) => {
+    const _id = req.params._id;
+    const admin = req.user._id;
+    const updatedChannel = req.body;
+    if (!updatedChannel) {
+        return res.status(400).json({message: "Please provide a channel name"})
+    }
+    try {
+        const channel = await Channel.findOne({_id, admin})
+        if (!channel) {
+            return res.status(401).json({message: 'Unauthorized to update this'})
+        }
+        const options = {new: true};
+        const result = await Channel.findByIdAndUpdate(
+            _id,
+            {channelName: updatedChannel},
+            options
+        )
+        channelCache.del(`latestChannel:${admin}`)
+        channelCache.del(`channels:${admin}`)
+        channelCache.del(`channelById:${admin}`)
+        return res.status(200).json({message: 'Channel name updated !', result})
+
+    } catch (err) {
+        return res.status(500).json({message: `Internal server error ${err}`})
+    }
+})
+
+exports.deleteChannel = asyncErrorHandler(async (req, res, next) => {
+    const _id = req.params._id;
+    const admin = req.user._id;
+    try {
+        const findSpace = await Channel.findOne({_id, admin});
+        if (!findSpace) {
+            return res.status(403).json({message: 'Unauthorized to delete the channel'})
+        }
+        const deleteSpace = await Channel.findByIdAndDelete({_id, admin});
+        channelCache.del(`latestChannel:${admin}`)
+        channelCache.del(`channels:${admin}`)
+        channelCache.del(`channelById:${admin}`)
+        return res.status(200).json({message: "Channel deleted ", deleteSpace})
+    } catch (err) {
+        return res.status(501).json({message: `Internal server error ${err}`})
+    }
+})
